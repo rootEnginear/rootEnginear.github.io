2f6b68dce01842f3e82f9116276a4bc8f09bb4ea
rootEnginear <rootEnginear@users.noreply.github.com>
2023-04-06 17:54:06 +0700

    feat: add some contents

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -8,17 +9,29 @@ module.exports = {
       sans: ["Space Grotesk Variable", ...defaultTheme.fontFamily.sans],
       serif: ["Space Grotesk Variable", ...defaultTheme.fontFamily.serif],
     },
-    spacing: {},
+    spacing: {
+      0: "0",
+      1: "1px",
+      2: "2px",
+      4: "4px",
+      8: "8px",
+      16: "16px",
+      32: "32px",
+      64: "64px",
+      "1rem": "1rem",
+    },
     colors: {
       transparent: "transparent",
       current: "currentColor",
       black: "#000",
       white: "#fff",
+      gray: colors.zinc,
     },
     borderWidth: {
       DEFAULT: "1px",
     },
     borderRadius: {
+      16: "16px",
       full: "9999px",
     },
     extend: {},
