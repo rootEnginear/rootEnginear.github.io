aec0bc1776b9f107f86f2c95f24377285127666c
rootEnginear <rootEnginear@users.noreply.github.com>
2023-04-06 15:37:11 +0700

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rootenginear.github.io",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ["https://rootenginear.github.io/25th-parliament-meeting/"],
+    }),
+  ],
 });
