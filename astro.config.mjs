c1524b60301c7fbe94b621d9fef4cffe7c3b250c
rootEnginear <rootEnginear@users.noreply.github.com>
2023-04-06 15:26:02 +0700

    chore: add tailwinds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rootenginear.github.io'
+  site: "https://rootenginear.github.io",
+  integrations: [tailwind()],
 });
