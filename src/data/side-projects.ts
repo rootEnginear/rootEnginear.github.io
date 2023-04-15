f325685750053f515d83c73de90a4d47bf55b9cb
rootEnginear <rootEnginear@users.noreply.github.com>
2023-04-15 07:36:14 +0700

    fix: extract project cards, link title

---
+++ src/data/side-projects.ts
@@ -0,0 +1,32 @@
+export const SIDE_PROJECTS = [
+  {
+    title: "Two Point Campus Course Planner",
+    description:
+      "This tool will help you to plan your perfect course combination for your campus. This is done with a simple mathematical calculation. In practice, however, you might have to fight with your timetable scheduler.",
+    src: "./img/projects/tpc-course-planner.webp",
+    website: "https://rootenginear.github.io/tpc-course-planner",
+    github: "https://github.com/rootEnginear/tpc-course-planner",
+  },
+  {
+    title: "25<sup>th</sup> Parliament Meetings",
+    description:
+      "A collection of meeting records from the 25<sup>th</sup> parliament (also known as the Prayuth period). This solves the searching problem on the Parliament website, so you can find any meeting records faster.",
+    src: "./img/projects/25th-parliament-meetings.webp",
+    website: "https://rootenginear.github.io/25th-parliament-meeting",
+    github: "https://github.com/rootEnginear/25th-parliament-meeting",
+  },
+  {
+    title: "My Common Patterns",
+    description: "My personal &ldquo;frequently copy&rdquo; code snippets and patterns.",
+    src: "./img/projects/my-common-patterns.webp",
+    website: "https://rootenginear.github.io/my-common-patterns",
+    github: "https://github.com/rootEnginear/my-common-patterns",
+  },
+  {
+    title: "Libraries (that I have used) Review",
+    description:
+      "My subjective review on libraries I have used and what I have experienced from them.",
+    src: "./img/projects/lib-reviews.webp",
+    github: "https://github.com/rootEnginear/libraries-i-used-review",
+  },
+];
