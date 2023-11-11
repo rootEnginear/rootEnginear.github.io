c7eb89cfc99266e7db26f00fc713cf8d6ff6b150
rootEnginear <rootEnginear@users.noreply.github.com>
2023-11-11 08:47:15 +0700

    feat: add bta mods to side projects

--- src/data/side-projects.ts
+++ src/data/side-projects.ts
@@ -1,5 +1,13 @@
 export const SIDE_PROJECTS = [
   {
+    title: "Minecraft (Better Than Adventure) Mods",
+    description:
+      "Better Than Adventure (BTA) is a popular Minecraft mod that depicts what Minecraft would be like if the adventure update didn't take place. I mod it further with new features and fun stuff for players to enjoy.",
+    src: "./img/projects/bta-mods.webp",
+    website: "https://bta-modding.nouma-vallee.fr/user/rootEnginear",
+    github: "https://github.com/rootEnginear/bta-rootenginear-mods",
+  },
+  {
     title: "Two Point Campus Course Planner",
     description:
       "This tool will help you to plan your perfect course combination for your campus. This is done with a simple mathematical calculation. In practice, however, you might have to fight with your timetable scheduler.",
@@ -8,12 +16,11 @@ export const SIDE_PROJECTS = [
     github: "https://github.com/rootEnginear/tpc-course-planner",
   },
   {
-    title: "25<sup>th</sup> Parliament Meetings",
-    description:
-      "A collection of meeting records from the 25<sup>th</sup> parliament (also known as the Prayuth period). This solves the searching problem on the Parliament website, so you can find any meeting records faster.",
+    title: "26<sup>th</sup> Parliament Meetings",
+    description: `A collection of meeting records from the 26<sup>th</sup> parliament. This solves the searching problem on the Parliament website, so you can find any meeting records faster.<br><span class="text-sm">Also see: <a href="https://rootenginear.github.io/25th-parliament-meeting" target="_blank" rel="nofollow noopener noreferrer">https://rootenginear.github.io/25th-parliament-meeting</a></span>`,
     src: "./img/projects/25th-parliament-meetings.webp",
-    website: "https://rootenginear.github.io/25th-parliament-meeting",
-    github: "https://github.com/rootEnginear/25th-parliament-meeting",
+    website: "https://rootenginear.github.io/26th-parliament-meeting",
+    github: "https://github.com/rootEnginear/26th-parliament-meeting",
   },
   {
     title: "My Common Patterns",
@@ -22,11 +29,11 @@ export const SIDE_PROJECTS = [
     website: "https://rootenginear.github.io/my-common-patterns",
     github: "https://github.com/rootEnginear/my-common-patterns",
   },
-  {
-    title: "Libraries (that I have used) Review",
-    description:
-      "My subjective review on libraries I have used and what I have experienced from them.",
-    src: "./img/projects/lib-reviews.webp",
-    github: "https://github.com/rootEnginear/libraries-i-used-review",
-  },
+  // {
+  //   title: "Libraries (that I have used) Review",
+  //   description:
+  //     "My subjective review on libraries I have used and what I have experienced from them.",
+  //   src: "./img/projects/lib-reviews.webp",
+  //   github: "https://github.com/rootEnginear/libraries-i-used-review",
+  // },
 ];
