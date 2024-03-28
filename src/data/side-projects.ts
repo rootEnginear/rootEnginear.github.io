083e197b440daead80635f02ae8e87043bbc5e9e
rootEnginear <rootEnginear@users.noreply.github.com>
2024-03-28 20:06:50 +0700

    fix: absolute img path

--- src/data/side-projects.ts
+++ src/data/side-projects.ts
@@ -3,7 +3,7 @@ export const SIDE_PROJECTS = [
     title: "Minecraft (Better Than Adventure) Mods",
     description:
       "Better Than Adventure (BTA) is a popular Minecraft mod that depicts what Minecraft would be like if the adventure update didn't take place. I mod it further with new features and fun stuff for players to enjoy.",
-    src: "./img/projects/bta-mods.webp",
+    src: "/img/projects/bta-mods.webp",
     website: "https://bta-modding.nouma-vallee.fr/user/rootEnginear",
     github: "https://github.com/rootEnginear/bta-rootenginear-mods",
   },
@@ -11,21 +11,21 @@ export const SIDE_PROJECTS = [
     title: "Two Point Campus Course Planner",
     description:
       "This tool will help you to plan your perfect course combination for your campus. This is done with a simple mathematical calculation. In practice, however, you might have to fight with your timetable scheduler.",
-    src: "./img/projects/tpc-course-planner.webp",
+    src: "/img/projects/tpc-course-planner.webp",
     website: "https://rootenginear.github.io/tpc-course-planner",
     github: "https://github.com/rootEnginear/tpc-course-planner",
   },
   {
     title: "26<sup>th</sup> Parliament Meetings",
     description: `A collection of meeting records from the 26<sup>th</sup> parliament. This solves the searching problem on the Parliament website, so you can find any meeting records faster.<br><span class="text-sm">Also see: <a href="https://rootenginear.github.io/25th-parliament-meeting" target="_blank" rel="nofollow noopener noreferrer">https://rootenginear.github.io/25th-parliament-meeting</a></span>`,
-    src: "./img/projects/25th-parliament-meetings.webp",
+    src: "/img/projects/25th-parliament-meetings.webp",
     website: "https://rootenginear.github.io/26th-parliament-meeting",
     github: "https://github.com/rootEnginear/26th-parliament-meeting",
   },
   {
     title: "My Common Patterns",
     description: "My personal &ldquo;frequently copy&rdquo; code snippets and patterns.",
-    src: "./img/projects/my-common-patterns.webp",
+    src: "/img/projects/my-common-patterns.webp",
     website: "https://rootenginear.github.io/my-common-patterns",
     github: "https://github.com/rootEnginear/my-common-patterns",
   },
