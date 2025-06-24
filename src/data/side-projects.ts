47d39a06e0a425ede345af11e3cdbb9aee261628
rootEnginear <rootenginear@users.noreply.github.com>
2025-06-24 18:29:04 +0700

    feat: svelte action motionone

--- src/data/side-projects.ts
+++ src/data/side-projects.ts
@@ -1,5 +1,12 @@
 export const SIDE_PROJECTS = [
   {
+    title: "Svelte Action MotionOne",
+    description: "Unofficial Svelte Action for Motion One animation library.",
+    src: "/img/projects/svelte-action-motionone.webp",
+    website: "https://rootenginear.github.io/svelte-action-motionone",
+    github: "https://github.com/rootEnginear/svelte-action-motionone",
+  },
+  {
     title: "Minecraft (Better Than Adventure) Mods",
     description:
       "Better Than Adventure (BTA) is a popular Minecraft mod that depicts what Minecraft would be like if the adventure update didn't take place. I mod it further with new features and fun stuff for players to enjoy.",
@@ -8,19 +15,19 @@ export const SIDE_PROJECTS = [
     github: "https://github.com/rootEnginear/bta-rootenginear-mods",
   },
   {
+    title: "Saparchive",
+    description: `A collection of meeting records from the 25<sup>th</sup> and 26<sup>th</sup> parliament. This solves the searching problem on the Parliament website, so you can find any meeting records faster.`,
+    src: "/img/projects/saparchive.webp",
+    website: "https://saparchive-th.web.app",
+    github: "https://github.com/rootEnginear/saparchive",
+  },
+  {
     title: "Two Point Campus Course Planner",
     description:
       "This tool will help you to plan your perfect course combination for your campus. This is done with a simple mathematical calculation. In practice, however, you might have to fight with your timetable scheduler.",
     src: "/img/projects/tpc-course-planner.webp",
     website: "https://rootenginear.github.io/tpc-course-planner",
     github: "https://github.com/rootEnginear/tpc-course-planner",
-  },
-  {
-    title: "Saparchive",
-    description: `A collection of meeting records from the 25<sup>th</sup> and 26<sup>th</sup> parliament. This solves the searching problem on the Parliament website, so you can find any meeting records faster.`,
-    src: "/img/projects/saparchive.webp",
-    website: "https://saparchive-th.web.app",
-    github: "https://github.com/rootEnginear/saparchive",
   },
   {
     title: "My Common Patterns",
